ef9f1650423b8c552fd896c76ace40733589c9e6
Eraly <81900112+Chambo015@users.noreply.github.com>
2024-04-19 14:39:11 +0500

    new dates

--- src/components/TimerApp.vue
+++ src/components/TimerApp.vue
@@ -1,5 +1,10 @@
 <template>
     <div id="timer">
+        <div class="buttons">
+            <button v-for="(d, idx) of dates" :key="d.date" :class="['btn', idx === currentDateId && 'active']" @click="() => currentDateId = idx" >
+                {{ d.title }}
+            </button>
+        </div>
         <div id="timer-text">
             <TimerChar v-for="(char, index) of getTime" :key="index" :char="char" />
         </div>
@@ -9,22 +14,17 @@
 <script setup lang="ts">
 import { computed, onMounted, onUnmounted, ref } from 'vue'
 import TimerChar from './TimerChar.vue'
+import {dates} from '../data/index'
 
 const images = ['https://sun9-30.userapi.com/impg/NVnWqKYXENu9zIgnkCG4sa58XDkReCfGvn8tqA/zIn0vVXphT8.jpg?size=1800x1200&quality=96&sign=d906c99c65633807aa1430adde7e9058&type=album', ]
 
-const props = defineProps({
-    date: {
-        type: String,
-        default: '2023-07-01T02:40:00'
-    }
-})
-
+const currentDateId = ref<number>(0)
 const formatSegment = (segment: number): string => {
     return segment < 10 ? `0${segment}` : `${segment}`
 }
 
 const nowDate = ref<Date>(new Date())
-const timeDiff = computed(() => Math.abs(nowDate.value.getTime() - new Date(props.date).getTime()))
+const timeDiff = computed(() => Math.abs(nowDate.value.getTime() - new Date(dates[currentDateId.value].date).getTime()))
 const getTime = computed(() => {
     const days = Math.floor(timeDiff.value / (1000 * 3600 * 24)),
         hours = Math.floor(timeDiff.value / (1000 * 3600)) % 24,
